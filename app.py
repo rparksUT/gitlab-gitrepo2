2f0f55a8c74687b2fb1b6ab2cdca635453781210
Rick Parks <ntisRP30@pgac.com>
2021-02-08 15:55:06 -0600

    Add feature branch #A updates

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ def main():
     letter_count = get_letter_count(phrase)
 
     print('"{0:s}" contains {1:d} vowels'.format(phrase, vowel_count))
-    print('"{0:s}" contains the following letter counts:'.format(phrase))
+    print('"{0:s}" contains the following letter counts (feature branch A):'.format(phrase))
     for item in letter_count:
         print('{0:d} occurrence(s) of {1:s}'.format(letter_count[item], item))
 
